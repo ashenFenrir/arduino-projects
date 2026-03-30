2b92ad7837e774e82207d0951289736b9a92dea8
Yan Odnorob <119529146+ashenFenrir@users.noreply.github.com>
2026-03-30 07:42:16 +0200

    feat: a lot of stuff
    + add a potentiometer
    + add edit, break, pause and work modes
    + add buzzer
    + add second button
    + add support for different push types(click, hold)

--- pomodoro/pomodoro.ino
+++ pomodoro/pomodoro.ino
@@ -16,25 +16,21 @@ byte F = 4;
 byte G = 3;
 byte DP = 5; // decimal point
 
+
 int latchPin = 27;
-//Pin connected to SH_CP(11) blue of 74HC595
 int clockPin = 26;
-////Pin connected to DS of 74HC595
 int dataPin = 25;
+
+int potentiometer = 39;
+
+int buzzer = 32;
 
 void control(int n){
   digitalWrite(latchPin, LOW);
-  // shift out the bits:
   shiftOut(dataPin, clockPin, MSBFIRST, n);
-  //take the latch pin high so the LEDs will light up:
   digitalWrite(latchPin, HIGH);
 }
 
-byte pot = 15;
-
-const byte button1 = 35;
-
-// Define the input pins for the 7-segment display segments.
 const int segmentPins[] = {A, B, C, D, E, F, G, DP};
 
 const bool zero[] =       {1, 1, 1, 1, 1, 1, 0};
@@ -47,280 +43,272 @@ const bool six[] =        {1, 0, 1, 1, 1, 1, 1};
 const bool seven[] =      {1, 1, 1, 0, 0, 0, 0};
 const bool eight[] =      {1, 1, 1, 1, 1, 1, 1};
 const bool nine[] =       {1, 1, 1, 1, 0, 1, 1};
-const bool* numbers[] = {zero, one, two, three, four, five, six, seven, eight, nine};
+const bool letter_A[] =   {1, 1, 1, 0, 1, 1, 1};
+const bool letter_P[] =   {1, 1, 0, 0, 1, 1, 1};
+const bool letter_H[] =   {0, 1, 1, 0, 1, 1, 1};
+const bool letter_E[] =   {1, 0, 0, 1, 1, 1, 1};
+const bool* numbers[] = {zero, one, two, three, four, five, six, seven, eight, nine, letter_P, letter_A, letter_H , letter_E};
 
-// Define the input pins for the 7-segment display digits.
 const int digitPins[] = {D1, D2, D3, D4, DS};
 
-// For common cathode
-uint8_t sdigitON = LOW;
-uint8_t sdigitOFF = HIGH;
-
 uint8_t digitON = HIGH;
-uint8_t digitOFF = LOW;
+uint8_t digitOFF = !digitON;
 
-uint8_t segmentON = HIGH;
-uint8_t segmentOFF = LOW;
+uint8_t segmentON = LOW;
+uint8_t segmentOFF = !segmentON;
 
-uint8_t ssegmentON = HIGH;
-uint8_t ssegmentOFF = LOW;
-
-enum Mode {
-    RUNNING_WORK,
-    RUNNING_BREAK,
-    PAUSE,
-    EDIT1,
-    EDIT2
-};
-
-int workTime = 10;
-int breakTime = 5;
-
-int currentWorkTime = workTime;
-int currentBreakTime = breakTime;
-
-unsigned long modeStartTime;
-unsigned long button1Time;
-
-enum Mode mod;
-enum Mode pauseFromMode;
-
-float dot_running_speed = 2;
-
-
+int buttons[] = {34, 35};
 
 void setup() {
-  // Pin initialization.
-//  for (int i = 0; i < 8; i++) {
-//    pinMode(segmentPins[i], OUTPUT);
-//    //digitalWrite(segmentPins[i], segmentOFF);
-//  }
-
   for (int i = 0; i < 5; i++) {
     pinMode(digitPins[i], OUTPUT);
-    digitalWrite(digitPins[i], i!=4?digitON:sdigitON);
+    digitalWrite(digitPins[i], digitOFF);
   }
-  
-//
-//  //potentiometer
-//  pinMode(pot, INPUT);
-//
-//  pinMode(button1, INPUT);
+  for(int i = 0; i < 2; i++){
+    pinMode(buttons[i], INPUT);
+  }
 
+  pinMode(potentiometer, INPUT);
+  
   pinMode(latchPin, OUTPUT);
   pinMode(clockPin, OUTPUT);
   pinMode(dataPin, OUTPUT);
 
-  Serial.begin(9600);
+  pinMode(buzzer, OUTPUT);
   
-  mod = PAUSE;
-  pauseFromMode = RUNNING_WORK;
-  modeStartTime = millis();
-  button1Time = -1;
+  Serial.begin(9600);
 }
-
-
-unsigned long buttonImpulseTime;
-unsigned long buttonCompleteTime;
-bool buttonMomentState;
-bool buttonGlobalState;
-
-enum BUTTON_EVENT{
-  NO_EVENT,
-  PRESS_SINGLE,
-  PRESS_DOUBLE,
-  PRESS_HOLD
-};
-//0 0
-BUTTON_EVENT get_button_event();//damn compiler, it placed the prototype at the top of them code... which why the declaration of the BUTTON_EVENT enum was out of the scope further down the code
-BUTTON_EVENT get_button_event(){
-  if((analogRead(button1)>4000) != buttonMomentState){    //if state changed...
-    if(buttonGlobalState){                          //  if it's start or end of the bounce...
-      if(!(analogRead(button1)>4000)){                    //    if it's the start of the bounce...
-        buttonImpulseTime = millis();               //      start count down
-        buttonMomentState = false;                  //      and set the state to 0
-      }else{                                        //    if it's the start of the bounce...
-        if(millis()-buttonImpulseTime > 50){        //      if it was up for longer than 50ms...
-          buttonGlobalState = false;
-          buttonMomentState = false;
-          return PRESS_SINGLE;                      //        then it's a single press!
-        }
-      }
-    }
-    else{                                           //first press down
-      buttonGlobalState = true;
-      buttonMomentState = true;
-    }
-  }
-  return NO_EVENT;
-}
-
 
 void show_digit(int n, bool point, bool high){
   int mask = 0;
   for(int i = 0; i < 7; i++){
-    int smth = (numbers[n][i]?high:!high)<<(7-segmentPins[i]);
+    int smth = (!numbers[n][i]^high)<<(7-segmentPins[i]);
     mask |= smth;
-    //printf("m %08b\n", smth);
-    //digitalWrite(segmentPins[i], numbers[n][i]?segmentON:segmentOFF);
+    mask |= (!high^point)<<(7-DP);
+    control(mask);
   }
-  //printf("%i ", n);
-  //printf("%08b\n", mask);
-  mask |= (high^point)<<(7-DP);
-  control(mask);
 }
 
 void show_digits(int* digits, bool *points){
-//  int a = n/1000;
-//  int b = n/100%10;
-//  int c = n/10%10;
-//  int d = n%10;
-//
-//  int digits[] = {a, b, c, d};
-
   for(int i = 0; i < 5; i++){
     if(digits[i] != -1){
-      show_digit(digits[i], !points[i], i==4);      
-      digitalWrite(digitPins[i], i!=4?segmentON:ssegmentON);
-    }
-    else
-      digitalWrite(digitPins[i], i!=4?segmentOFF:ssegmentOFF);
+      show_digit(digits[i], points[i], segmentON^i==4);      
+      digitalWrite(digitPins[i], digitON);
+    } else
+      digitalWrite(digitPins[i], digitOFF);
       
-    digitalWrite(digitPins[(i+1)%5], ((i+1)%5)!=4?segmentOFF:ssegmentOFF);
-    digitalWrite(digitPins[(i+2)%5], ((i+2)%5)!=4?segmentOFF:ssegmentOFF);
-    digitalWrite(digitPins[(i+3)%5], ((i+3)%5)!=4?segmentOFF:ssegmentOFF);
-    digitalWrite(digitPins[(i+4)%5], ((i+4)%5)!=4?segmentOFF:ssegmentOFF);
-    delay(2);
+    digitalWrite(digitPins[(i+1)%5], digitOFF);
+    digitalWrite(digitPins[(i+2)%5], digitOFF);
+    digitalWrite(digitPins[(i+3)%5], digitOFF);
+    digitalWrite(digitPins[(i+4)%5], digitOFF);
+    delay(4);
+    digitalWrite(digitPins[(i)%5], digitOFF);
   }
 }
 
+enum Mode {
+  WORK,
+  BREAK,
+  PAUSE,
+  EDIT
+};
 
+enum Mode prev_mode = WORK;
+enum Mode mod = PAUSE;
 
-void show_pause_mode(){
-  unsigned long elapsedTime = millis() - modeStartTime;
-//  int n = floor((float)analogRead(pot)/4095*60);
-  //Serial.println((float)analogRead(pot)/4095*60);
+int button_state[2];
+unsigned long button_times[2] = {0, 0};
 
-  int a = currentWorkTime/10;
-  int b = currentWorkTime%10;
-  int c = currentBreakTime/10;
-  int d = currentBreakTime%10;
+//for dubble and triple push
+//int button_streak[2] = {0, 0};
+//unsigned long button_streak_times[2] = {0, 0};
 
-  int digits[] = {a, b, c, d};
-  
-  //show_digits(digits, int(elapsedTime*dot_running_speed/1000)%3+1);
-
-   if(get_button_event() == PRESS_SINGLE){
-//    if(button1Time == -1)
-//    {
-      mod = pauseFromMode;
-
-//      button1Time = millis();
-      modeStartTime = millis();
-      return;
+void update_stuff(){
+  for(int i = 0; i < 2; i++){
+    if(analogRead(buttons[i]) > 4000){
+      if(button_times[i] == 0){
+        button_state[i] = 1;
+        button_times[i] = millis();
+      }
+    }else {
+      if(button_times[i] && millis()-button_times[i] > 1000){
+        button_state[i] = 3;
+      } else if(button_times[i]){
+        button_state[i] = 2;
+      } else{
+        button_state[i] = 0;  
+      }
+      button_times[i] = 0;
     }
-//  }else
-//    button1Time=-1;
+  }
 }
+
+unsigned int default_work_time = 10;
+unsigned int default_break_time = 5;
+
+unsigned long ramaining_work_time = 10;
+unsigned long ramaining_break_time = 5;
+
+unsigned long this_work_time = 10;
+unsigned long this_break_time = 5;
+
 
 bool demo = true;
 
-void show_work_mode(){
-  unsigned long elapsedTime = millis() - modeStartTime;
+void show_time(int a, int b, int mode_letter){
+  int digits[] = {a/10, a%10, b/10, b%10, mode_letter};
+  bool points[] = {false, true, false, false, false};
+  show_digits(digits, points);
+}
 
-  int displayTime = currentWorkTime-elapsedTime/(1000*(1+59*(!demo)));
+unsigned long start_time = 0;
 
-  if(displayTime<0){
-    mod = RUNNING_BREAK;
-    currentWorkTime = workTime;
-    modeStartTime = millis();
+
+void pause_mode(){
+  if(button_state[0] == 2){
+    mod = prev_mode;
+    start_time = millis();
     return;
   }
-
-  if(get_button_event() == PRESS_SINGLE){
-//    if(button1Time == -1)
-//    {
-      pauseFromMode = RUNNING_WORK;
-      mod = PAUSE;
-      currentWorkTime = displayTime;
-      
-//      button1Time = millis();
-      modeStartTime = millis();
-      return;
-    }
-//  }else
-//    button1Time=-1;
-  
-  int a = displayTime/10;
-  int b = displayTime%10;
-  int c = currentBreakTime/10;
-  int d = currentBreakTime%10;
-
-  int digits[] = {a, b, c, d};
-  
-  //show_digits(digits, 2);
-
-  
-}
-void show_break_mode(){
-  unsigned long elapsedTime = millis() - modeStartTime;
-
-  int displayTime = currentBreakTime-elapsedTime/(1000*(1+59*(!demo)));
-
-  if(displayTime<0){
-    mod = RUNNING_WORK;
-    currentBreakTime = breakTime;
-    modeStartTime = millis();
-    return;
+  else if(button_state[1] == 2){
+    mod = EDIT;
+  }
+  else if(button_state[1] == 3){
+    ramaining_work_time = default_work_time;
+    ramaining_break_time = default_break_time;
+    this_work_time = default_work_time;
+    this_break_time = default_break_time;
   }
 
-  if(get_button_event() == PRESS_SINGLE){
-//    if(button1Time == -1)
-//    {
-      pauseFromMode = RUNNING_BREAK;
-      mod = PAUSE;
-      currentBreakTime = displayTime;
+  
 
-//      button1Time = millis();
-      modeStartTime = millis();
-      return;
+  show_time(ramaining_work_time, ramaining_break_time, millis()%1500>750?12:(prev_mode==WORK?11:10));
+}
+
+void edit_mode(){
+  if(button_state[0] == 3){
+    if(prev_mode == WORK) prev_mode = BREAK;
+    else if(prev_mode == BREAK) prev_mode = WORK;
+  }
+  else if(button_state[1] == 2){
+    if(prev_mode == WORK){
+      default_work_time = analogRead(potentiometer)*60/4095;
+      prev_mode = BREAK;
     }
-//  }else
-//    button1Time=-1;
-  
-  int a = currentWorkTime/10;
-  int b = currentWorkTime%10;
-  int c = displayTime/10;
-  int d = displayTime%10;
+    else if(prev_mode == BREAK){
+      default_break_time = analogRead(potentiometer)*60/4095;
+      prev_mode = WORK;
+    }
+  }
+  else if(button_state[1] == 3){
+    mod = PAUSE;
+  }
+  if(prev_mode == WORK){
+    
+    show_time(analogRead(potentiometer)*60/4095, default_break_time, millis()%1500>750?13:11);
+  }
+  else if(prev_mode == BREAK){
+    show_time(default_work_time, analogRead(potentiometer)*60/4095, millis()%1500<750?13:11);
+  }
+}
 
-  int digits[] = {a, b, c, d};
+void break_mode(){
+  if(ramaining_break_time == 0){
+    ramaining_break_time = default_break_time;
+    this_break_time = default_break_time;
+    start_time = millis();
+    mod = WORK;
+    return;
+  }
   
-//  show_digits(digits, 2);
+  if(button_state[0] == 2){
+    mod = PAUSE;
+    prev_mode = BREAK;
+    this_break_time = ramaining_break_time;
+    return;
+  }
+  else if(button_state[0] == 3){
+    ramaining_break_time = default_break_time;
+    this_break_time = default_break_time;
+    start_time = millis();
+    mod = WORK;
+    return;
+  }
+  else if(button_state[1] == 2){
+    mod = EDIT;
+    prev_mode = BREAK;
+  }
+  else if(button_state[1] == 3){
+    ramaining_break_time = default_break_time;
+    this_break_time = default_break_time;
+    start_time = millis();
+  }
+
+  if((millis()-start_time)<100){
+    tone(buzzer, 2000);
+  }
+  else
+    tone(buzzer, 0);
+  
+  ramaining_break_time = this_break_time-(millis()-start_time)/1000;
+  
+  show_time(ramaining_work_time, ramaining_break_time, 10);
 }
-void show_edit1_mode(){
-  unsigned long elapsedTime = millis() - modeStartTime;
-}
-void show_edit2_mode(){
-  unsigned long elapsedTime = millis() - modeStartTime;
+
+
+void work_mode(){
+  if(ramaining_work_time == 0){
+    ramaining_work_time = default_work_time;
+    this_work_time = default_work_time;
+    start_time = millis();
+    mod = BREAK;
+    return;
+  }
+  
+  if(button_state[0] == 2){
+    mod = PAUSE;
+    prev_mode = WORK;
+    this_work_time = ramaining_work_time;
+    return;
+  }
+  else if(button_state[0] == 3){
+    ramaining_work_time = default_work_time;
+    this_work_time = default_work_time;
+    start_time = millis();
+    mod = BREAK;
+    return;
+  }
+  else if(button_state[1] == 2){
+    mod = EDIT;
+    prev_mode = WORK;
+  }
+  else if(button_state[1] == 3){
+    ramaining_work_time = default_work_time;
+    this_work_time = default_work_time;
+    start_time = millis();
+  }
+  
+  ramaining_work_time = this_work_time-(millis()-start_time)/1000;
+  
+  show_time(ramaining_work_time, ramaining_break_time, 11);
 }
 
 void loop() {
-  for(int i = 0; i < 8; i++){
-    int digits[] = {1, 2, 3, 4, 5};
-    bool points[] = {true, false, true, false, true};
-//    control(1<<(7-i));
-//    delay(1000);
-//    
-    show_digits(digits, points);
+  update_stuff();
+  printf("%i\n", analogRead(potentiometer));
+
+  switch(mod){
+    case PAUSE:
+      pause_mode();
+      break;
+    case WORK:
+      work_mode();
+      break;
+    case BREAK:
+      break_mode();
+      break;
+    case EDIT:
+      edit_mode();
+      break;
   }
-//  Serial.print(get_button_event());
-//  Serial.print(" ");
-//  Serial.print((analogRead(button1)>4000));
-//  Serial.print(" ");
-//  Serial.println(analogRead(button1));
-//  switch(mod){
-//    case PAUSE: show_pause_mode(); break;
-//    case RUNNING_WORK: show_work_mode(); break;
-//    case RUNNING_BREAK: show_break_mode(); break;
-//  }
 }
